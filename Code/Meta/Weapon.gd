1f4d53d02d8d7f8b401f0fb3b013b17d0e79aa14
Reivax2003 <santiago25@up.edu>
2024-09-17 19:05:50 -0700

    Standardized weapon class
    Weapons now have a "label" variable like Enemies. They also have a deal_damage function which takes an Entity and float as arguments

--- Code/Meta/Weapon.gd
+++ Code/Meta/Weapon.gd
@@ -2,3 +2,10 @@ extends CharacterBody2D
 class_name Weapon
 
 var mirrored = false
+var label = ""
+
+func get_label():
+	return label
+
+func deal_damage(entity: Entity, amount: float):
+	entity.take_damage(amount, label)

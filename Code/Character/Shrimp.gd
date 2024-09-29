a8f0b15853cab80568180285bfdaea7c3e4cc653
santosty25 <santosty25@up.edu>
2024-09-28 23:06:25 -0700

    Added Shrimp Enemy
    Currently runs around randomly

--- Code/Character/Shrimp.gd
+++ Code/Character/Shrimp.gd
@@ -1,16 +1,21 @@
 extends Enemy
 class_name Shrimp
 
-const SPEED = 100
+const SPEED = 500
+const INITIAL_MOVE_TIME = 2
+const CHANGE_DIRECTION_TIME = 1
+
 var stepSide = false
-var target = Vector2.ZERO
+var direction = Vector2(0, -1) 
 var walkDist = 200
 var waitTimer = 3
 var maxWait = 5
 var minWait = 2
 var flipTimerMax = 0.1
 var flipTimer = 0
-var attack = 0
+var changeDirectionTimer = CHANGE_DIRECTION_TIME
+var initialMoveTimer = INITIAL_MOVE_TIME
+var hasSwitchedDirection = false
 
 # reference to the player
 var player
@@ -27,48 +32,33 @@ func _ready():
 func _process(delta):
 	super._process(delta)
 	if player:
-		# Set target to player's position
-		target = player.position
-		var direction = (target - position).normalized() * SPEED
-
-		flipTimer -= delta
-		if flipTimer <= 0:
-			stepSide = !stepSide
-			flipTimer = flipTimerMax
-
-		if (position - target).length() > 0.1:
-			if stepSide:
-				$AnimatedSprite2D.rotation_degrees = -10
-			else:
-				$AnimatedSprite2D.rotation_degrees = 10
-
-			if direction.x < 0:
-				scale.x = 0.3
-			elif direction.x > 0:
-				scale.x = -0.3
-
-			if (direction * delta).length() > (target - position).length():
-				move_and_collide(target - position)
-			else:
-				move_and_collide(direction * delta)
+		# check if the shrimp can start moving randomly
+		if not hasSwitchedDirection:
+			initialMoveTimer -= delta
+			if initialMoveTimer <= 0:
+				hasSwitchedDirection = true 
+				set_random_direction()
 		else:
-			$AnimatedSprite2D.rotation = 0
+			changeDirectionTimer -= delta
+			if changeDirectionTimer <= 0:
+				set_random_direction()  # change direction randomly
+				changeDirectionTimer = CHANGE_DIRECTION_TIME
+
+		# update the shrimp's position based on the current direction
+		move_and_collide(direction * SPEED * delta)
+
+		if direction.x < 0:
+			scale.x = 0.3
+			$AnimatedSprite2D.rotation_degrees = -10
+		else:
+			scale.x = -0.3
+			$AnimatedSprite2D.rotation_degrees = 10
+		
+		$AnimatedSprite2D.play("Idle")  
 	var bodies = $Area2D.get_overlapping_bodies()
-
-func _physics_process(delta):
-	attack -= delta
-	if attack < 0.2:
-		$AnimatedSprite2D.play("Idle")
-		# player.take_damage(1, "attack")
-		$HitEffect.show()
-	if attack < 0.1:
-		$HitEffect.hide()
-
-func _on_area_2d_body_entered(body):
-	$AnimatedSprite2D.play("Windup")
-	if body is Player:
-		body.take_damage(1, "attack")
-	attack = 0.3
-
+	
 func get_label():
 	return label
+
+func set_random_direction():
+	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
